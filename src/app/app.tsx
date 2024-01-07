faea527a5362f1dba44bd5ea25e81a4d93dcde6c
ZuevaDarya <zuevadasha55@gmail.com>
2024-01-07 21:44:20 +0300

    create app glossary

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import GlossaryPage from '../pages/glossary-page/glossary-page';
+import GraphPage from '../pages/graph-page/graph-page';
+
+function App() {
+  return (
+    <Routes>
+      <Route />
+      <Route />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/graph-page/graph-page.tsx
@@ -0,0 +1,8 @@
+function GraphPage() {
+  return(
+    <>
+    </>
+  );
+}
+
+export default GraphPage;
