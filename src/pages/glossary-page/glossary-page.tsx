f36b1261b1c89229ef43b9a482ff4fa2fbc42de5
ZuevaDarya <zuevadasha55@gmail.com>
2024-01-07 23:14:25 +0300

    create glossary-page

--- src/pages/glossary-page/glossary-page.tsx
+++ src/pages/glossary-page/glossary-page.tsx
@@ -1,7 +1,14 @@
+import Header from '../../components/header/header';
+import './glossary-page.css';
+
 function GlossaryPage() {
   return(
-    <>
-    </>
+    <div>
+      <Header />
+      <p>
+        Glossary
+      </p>
+    </div>
   );
 }
 
