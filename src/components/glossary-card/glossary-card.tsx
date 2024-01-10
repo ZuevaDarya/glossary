6010cc5cc3ec1296e73e4319003fca10316319e1
ZuevaDarya <zuevadasha55@gmail.com>
2024-01-11 00:44:28 +0300

    create glossary card component

--- src/components/glossary-card/glossary-card.tsx
+++ src/components/glossary-card/glossary-card.tsx
@@ -1,7 +1,19 @@
-function GlossaryCard() {
-  return(
-    <div>
+type GlossaryCardProps = {
+  termin: string;
+  definition: string;
+};
 
+function GlossaryCard({ termin, definition }: GlossaryCardProps) {
+  return(
+    <div className='glossary-card'>
+      <h2 className='glossary-card__termin'>
+        <dfn>
+          {termin}
+        </dfn>
+      </h2>
+      <p className='glossary-card__definition'>
+        {definition}
+      </p>
     </div>
   );
 }
