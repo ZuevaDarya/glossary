d725d882775e1424b975ac5f98ad41f3720770a0
ZuevaDarya <zuevadasha55@gmail.com>
2024-01-11 00:45:18 +0300

    create reactFlow graph

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
+import { ReactFlowProvider } from 'react-flow-renderer/nocss';
 import App from "./components/app/app.tsx";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/graph-page/graph-page.tsx
+++ src/pages/graph-page/graph-page.tsx
@@ -1,10 +1,39 @@
+import 
+ReactFlow,
+{
+  Background,
+  Controls, 
+  EdgeText,
+  MiniMap,
+  useEdgesState,
+  useNodesState,
+  BackgroundVariant
+} from 'react-flow-renderer';
 import Header from '../../components/header/header';
+import mindMap from '../../app-data/mindMap';
+import './graph-page.css';
 
 function GraphPage() {
+  const [nodes, , onNodesChange] = useNodesState(mindMap.nodes);
+  const [edges, , onEdgesChange] = useEdgesState(mindMap.edges);
+
   return(
     <>
       <Header />
-      Graph
+        <div className='graph-container'>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            fitView
+          >
+            <EdgeText x={0} y={0}/>
+            <Background variant={BackgroundVariant.Dots} />
+            <MiniMap />
+            <Controls showInteractive={true}/>
+          </ReactFlow>
+        </div>
     </>
   );
 }
